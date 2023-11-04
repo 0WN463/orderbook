080b6861a3364b973eddae0035ba80583b24bd43
0WN463 <>
2023-11-04 12:04:19 +0800

    Render initial snapshot

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
-import OrderBook from '@components/orderbook'
+import OrderBook from "@components/orderbook";
 
 export default function Home() {
+  // fetch from user account in future
+  const token = process.env.PUBLIC_JWT;
   return (
-  <main>
-  <OrderBook/>
+    <main>
+      <OrderBook token={token} />
     </main>
-  )
+  );
 }
--- src/components/orderbook.tsx
+++ src/components/orderbook.tsx
@@ -1,6 +1,32 @@
+"use client";
+import { Centrifuge } from "centrifuge";
+import { useState, useEffect } from "react";
 
-export default function Home() {
-  return (
-  <marquee> Hello, World! </marquee>
-  )
+export default function Orderbook({ token }: { token: string }) {
+  const [state, setState] = useState([]);
+
+  useEffect(() => {
+    const centrifuge = new Centrifuge("wss://api.prod.rabbitx.io/ws", {
+      token,
+    });
+
+    centrifuge.connect();
+    const sub = centrifuge.newSubscription("orderbook:BTC-USD");
+
+    sub.on("publication", (ctx) => {
+      //setState(s => [...s, ctx.data]);
+    });
+
+    sub.on("subscribed", (ctx) => {
+      setState(ctx.data);
+    });
+
+    sub.subscribe();
+
+    return () => centrifuge.disconnect();
+  }, [token]);
+
+  if (!token) return "Loading";
+
+  return <div> {JSON.stringify(state)} </div>;
 }
