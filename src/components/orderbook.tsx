2cd0dd29293a8e537dd918b9cffb498d0392e029
0WN463 <>
2023-11-04 12:43:12 +0800

    Render as table form

--- src/components/orderbook.tsx
+++ src/components/orderbook.tsx
@@ -2,8 +2,20 @@
 import { Centrifuge } from "centrifuge";
 import { useState, useEffect } from "react";
 
+const LIMIT = 10;
+
+type Order = [string, size]; // [price, size]
+
+type Orderbook = {
+  market: string;
+  bids: Order[];
+  asks: Order[];
+  sequence: number;
+  timestamp: number;
+};
+
 export default function Orderbook({ token }: { token: string }) {
-  const [state, setState] = useState([]);
+  const [state, setState] = useState<Orderbook>();
 
   useEffect(() => {
     const centrifuge = new Centrifuge("wss://api.prod.rabbitx.io/ws", {
@@ -18,7 +30,7 @@ export default function Orderbook({ token }: { token: string }) {
     });
 
     sub.on("subscribed", (ctx) => {
-      setState(ctx.data);
+      setState({ ...ctx.data, market: ctx.data.market_id });
     });
 
     sub.subscribe();
@@ -26,7 +38,56 @@ export default function Orderbook({ token }: { token: string }) {
     return () => centrifuge.disconnect();
   }, [token]);
 
-  if (!token) return "Loading";
+  if (!token || !state) return "Loading";
 
-  return <div> {JSON.stringify(state)} </div>;
+  return (
+    <>
+      <h1> {state.market} </h1>
+      Last updated: {new Date(state.timestamp / 1_000).toLocaleString()}
+      <table className="border-2">
+        <caption> Bids </caption>
+        <thead>
+          <tr className="border [&>*]:border">
+            <th>Price</th>
+            <th>Amount</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.bids
+            .toSorted((a, b) => b[0] - a[0])
+            .slice(0, LIMIT)
+            .map((b) => (
+              <tr key={b[0]} className="border [&>*]:border">
+                <td>{b[0]}</td>
+                <td>{b[1]}</td>
+                <td></td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+      <table className="border-2">
+        <caption> Asks </caption>
+        <thead>
+          <tr className="border">
+            <th>Price</th>
+            <th>Amount</th>
+            <th>Total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {state.asks
+            .toSorted((a, b) => b[0] - a[0])
+            .slice(0, LIMIT)
+            .map((b) => (
+              <tr key={b[0]} className="border [&>*]:border">
+                <td>{b[0]}</td>
+                <td>{b[1]}</td>
+                <td></td>
+              </tr>
+            ))}
+        </tbody>
+      </table>
+    </>
+  );
 }
